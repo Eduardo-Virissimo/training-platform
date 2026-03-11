a6f00a0399f7ccedc5e8bcffde6a013e62c76cca
Guilherme Queiroz Sassi <43296333886@udesc.br>
2026-03-11 08:53:58 -0300

    chore: revert prisma to version 6

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,25 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { filePreDelete } from './prismaExtensions/filePreDelete';
-
-function createPrismaClient() {
-  const dbUrl = new URL(process.env.DATABASE_URL!);
-
-  const adapter = new PrismaMariaDb({
-    host: dbUrl.hostname,
-    port: Number(dbUrl.port) || 3306,
-    user: dbUrl.username,
-    password: dbUrl.password,
-    database: dbUrl.pathname.slice(1),
-  });
-
-  return new PrismaClient({ adapter }).$extends(filePreDelete);
-}
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
